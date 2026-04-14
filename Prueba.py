5c09955f4cd9eeafbe74b747aa778388657c19fe
Seba <sebastianalban20@gmail.com>
2026-04-14 11:44:06 -0400

    primer commit

---
+++ Prueba.py
@@ -0,0 +1,19 @@
+# print("Ingrese su nombre")
+# nombre=input()
+# edad=int(input("Ingrese su edad: "))
+# print("Hola", nombre, "su edad en 5 años será", edad+5)
+
+
+# print("Ingrese el 1er numero: ")
+# num1=int(input())
+# print("Ingrese el 2do numero: ")
+# num2=int(input())
+
+# print("La suma de los dos numeros es: ", num1+num2)
+# print("La resta de los dos numeros es: ", num1-num2)
+
+
+# n1=int(input("Ingrese 1er numero: "))
+# n2=int(input("Ingrese 2do numero: "))
+# print("La suma de los dos numeros es: ", n1+n2)
+# print("La resta de los dos numeros es: ", n1-n2)

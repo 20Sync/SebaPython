b900e71f25abd189068c5cb19ed586f3ed19f843
Seba <sebastianalban20@gmail.com>
2026-04-14 12:50:38 -0400

    Prueba

--- Prueba.py
+++ Prueba.py
@@ -3,6 +3,7 @@
 # edad=int(input("Ingrese su edad: "))
 # print("Hola", nombre, "su edad en 5 años será", edad+5)
 
+# ========================================
 
 # print("Ingrese el 1er numero: ")
 # num1=int(input())
@@ -12,8 +13,66 @@
 # print("La suma de los dos numeros es: ", num1+num2)
 # print("La resta de los dos numeros es: ", num1-num2)
 
+# ========================================
 
 # n1=int(input("Ingrese 1er numero: "))
 # n2=int(input("Ingrese 2do numero: "))
 # print("La suma de los dos numeros es: ", n1+n2)
 # print("La resta de los dos numeros es: ", n1-n2)
+
+# ========================================
+
+#Menu uso del IF
+
+# print(
+# '''
+# 1.- Pera 1200
+# 2.- Manzana 1400
+# 3.- Piña 2000
+
+# '''
+# )
+
+# select=int(input("Seleccione una opción: "))
+
+# if select==1:
+#     print("El total a pagar es: ", 1200*1.19)
+# elif select==2:
+#     print("El total a pagar es: ", 1400*1.19)
+# elif select==3:
+#     print("El total a pagar es: ", 2000*1.19)
+# else:
+#     print("Numero no valido")
+
+# ========================================
+
+#Uso del FOR
+
+# 1)
+
+
+# for i in range(3):
+#     print("Repeticion N ", i+1)
+
+
+# 2)
+
+
+# num=int(input("Ingrese un numero: "))
+
+# for i in range (num):
+#     print(i+1, "Hola")
+
+
+# 3)
+
+
+# num=int(input("Ingrese un numero: "))
+
+# for i in range (10):
+#     print(num, " X ", i+1, " = ", num*(i+1))
+
+
+
+# ========================================
+
